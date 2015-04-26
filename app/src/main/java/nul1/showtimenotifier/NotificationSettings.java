ea0c76e85a70dac78dd47c573283e03bde0b5b75
joshuaahklein <joshuaahklein@gmail.com>
2015-04-26 16:12:03 -0400

    Modified notification setting activity, added checkboxes

--- app/src/main/java/nul1/showtimenotifier/NotificationSettings.java
+++ app/src/main/java/nul1/showtimenotifier/NotificationSettings.java
@@ -1,15 +1,19 @@
 package nul1.showtimenotifier;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.CheckBox;
+import android.view.View.OnClickListener;
 
 public class NotificationSettings extends ActionBarActivity {
+
+    private CheckBox chkWeek, chkDay, chkHour;
+    private Button SaveSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,27 @@ public class NotificationSettings extends ActionBarActivity {
                 //actv.setText("");
             }
         });
+
+        chkWeek   = (CheckBox) findViewById(R.id.radioButtonWeek);
+        chkDay     = (CheckBox) findViewById(R.id.radioButtonDay);
+        chkHour    = (CheckBox) findViewById(R.id.radioButtonHour);
+        SaveSettings = (Button) findViewById(R.id.button6);
+
+        SaveSettings.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //Save settings to DB on click
+
+                //Send user back to homepage
+                Intent intent = new Intent(NotificationSettings.this,Home_Screen.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
